2c2ad3f8b27e86ffaba7b04a915f76f493863474
zonk256 <asd05728@gmail.com>
2024-04-27 09:59:25 +0200

    Move frame data to another file

--- extra/img convert.py
+++ extra/img convert.py
@@ -1,21 +1,32 @@
 import os
 import cv2
 
-output = []
+color_array = []
+pixel_array = []
 
 for img in os.listdir("./"):
     if img.endswith(".png"):
-        hex_array = []
+
         image = cv2.imread(img)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # loop every pixel
         height, width, _ = image.shape
         for y in range(height):
             for x in range(width):
-                # combine rgb to single hex
-                hex_array.append("0x" + format(image[y, x][0], "02x") + format(image[y, x][1], "02x") + format(image[y, x][2], "02x"))
-        output.append(hex_array)
+                # get pixel color
+                color = image[y, x]
+                #convert color list to single value in 565 format
+                color_hex = hex((color[0] >> 3) << 11 | (color[1] >> 2) << 5 | (color[2] >> 3))
+                
+                if color_hex not in color_array:
+                    color_array.append(color_hex)
+                pixel_array.append(color_array.index(color_hex))
 
-with open("output.txt", "w") as f:
-    # write to file, stripping ', replace [ and ] with { and }
-    f.write("{" + str(output).strip("[]").replace("'", "").replace("[", "{").replace("]", "}") + "}")
+print(f" len color_array: {len(color_array)} -> {color_array[:10]}...")
+print(f" len pixel_array: {len(pixel_array)} -> {pixel_array[:10]}...")
+
+
+with open("../src/frame_data.cpp", "w") as f:
+    # write to file, strip ', replace [ and ] with { and }
+    f.write(f"""const uint16_t color_array[] = {{{str(color_array)[1:-1].replace("'","")}}};\n""")
+    f.write(f"""const uint16_t pixel_array[] = {{{str(pixel_array)[1:-1]}}};\n""")
---
+++ src/frame_data.h
@@ -0,0 +1,2 @@
+extern const uint16_t color_array[];
+extern const uint16_t pixel_array[];
